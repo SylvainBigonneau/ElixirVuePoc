f562e5307bae208ddb2877b77425082956a7a659
Sylvain BIGONNEAU <sbigonneau@sedona.fr>
2017-01-27 12:00:40 +0100

    ENH : load data before showing edit page

--- web/static/components/pages/edit.vue
+++ web/static/components/pages/edit.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+    import Vue from 'vue'
     import TopicForm from '../forms/topic-form.vue'
 
     export default {
@@ -13,18 +14,26 @@
                 topic: {}
             }
         },
+        beforeRouteEnter(to, from, next) {
+            let resource = Vue.resource('topics{/id}');
+            resource.get({ id: to.params.id }).then((response) => {
+                next(vm => {
+                    vm.topic = response.body;
+                })
+            });
+        },
         created() {
             this.resource = this.$resource('topics{/id}');
-            // fetch the data when the view is created and the data is
-            // already being observed
-            this.fetchData();
         },
-        methods: {
-            fetchData() {
+        watch: {
+            $route() {
+                this.topic = {}
                 this.resource.get({ id: this.id }).then((response) => {
                     this.topic = response.body;
-                });
-            },
+                })
+            }
+        },
+        methods: {
             submitTopic(data) {
                 this.resource.update({ id: this.id }, { topic: data }).then((response) => {
                     this.$router.push({ name: 'home' });
